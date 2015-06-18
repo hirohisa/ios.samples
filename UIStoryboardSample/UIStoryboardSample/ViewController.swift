85fb41f7b371f806c8b7a6d93f8f5b44314ff0a3
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2015-06-18 18:43:13 +0900

    Add UIStoryboardSample

---
+++ UIStoryboardSample/UIStoryboardSample/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  UIStoryboardSample
+//
+//  Created by Hirohisa Kawasaki on 6/18/15.
+//  Copyright (c) 2015 Hirohisa Kawasaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func change(segue: UIStoryboardSegue) {
+
+    }
+
+}
+
