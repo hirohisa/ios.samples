36f2ede59c50148eea8c5376f3f99f11743b3ff6
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2015-06-20 23:19:57 +0900

    Update UINavigationControllerSample
    Use Custom NavigationBar in storyboard

---
+++ UINavigationControllerSample/UINavigationControllerSample/NavigationBar.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationBar.swift
+//  UINavigationControllerSample
+//
+//  Created by Hirohisa Kawasaki on 6/20/15.
+//  Copyright (c) 2015 Hirohisa Kawasaki. All rights reserved.
+//
+
+import UIKit
+
+class NavigationBar: UINavigationBar {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        println("called")
+    }
+}
