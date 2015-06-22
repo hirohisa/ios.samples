8484082942eef70d0d6d3aa60377bc5be3052d5d
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2015-06-22 12:37:30 +0900

    Update NavigationBar in UINavigationControllerSample

--- UINavigationControllerSample/UINavigationControllerSample/NavigationBar.swift
+++ UINavigationControllerSample/UINavigationControllerSample/NavigationBar.swift
@@ -2,31 +2,32 @@
 //  NavigationBar.swift
 //  UINavigationControllerSample
 //
-//  Created by Hirohisa Kawasaki on 6/20/15.
 //  Copyright (c) 2015 Hirohisa Kawasaki. All rights reserved.
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 import UIKit
 
 class NavigationBar: UINavigationBar {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-
-    }
-
-    // if self is gone then call `setItems(items:animated:)`
-    /*
-    override func setItems(items: [AnyObject]!, animated: Bool) {
-        super.setItems(items, animated: animated)
-        println("set items")
-        println(items)
-    }
-    */
-
     override func layoutSubviews() {
         super.layoutSubviews()
-        println(backItem)
 
         if needsChangeBackBarButtonItem() {
             changeBackBarButtonItem()
