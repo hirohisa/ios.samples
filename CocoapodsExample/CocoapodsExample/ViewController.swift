ca48d7d5f661ca51f687e010cb52519c8cef8923
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2015-08-18 03:40:16 +0900

    Add CocoapodsExample

---
+++ CocoapodsExample/CocoapodsExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CocoapodsExample
+//
+//  Created by Hirohisa Kawasaki on 8/18/15.
+//  Copyright (c) 2015 Hirohisa Kawasaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
