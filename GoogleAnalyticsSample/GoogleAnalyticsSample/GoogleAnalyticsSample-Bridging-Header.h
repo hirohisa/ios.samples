c92dd26ce723e7d08e2f19499abb5977c165a7e8
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2015-10-21 19:10:16 +0900

    Add GoogleAnalyticsSample

---
+++ GoogleAnalyticsSample/GoogleAnalyticsSample/GoogleAnalyticsSample-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <GoogleAnalytics/GAI.h>
+#import <GoogleAnalytics/GAIFields.h>
+#import <GoogleAnalytics/GAILogger.h>
+#import <GoogleAnalytics/GAIDictionaryBuilder.h>
